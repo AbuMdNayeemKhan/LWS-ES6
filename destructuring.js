370cabe9bef1b27617f9d55828976e8d5260b0cc
unknown <abumohammadnayemkhan@gmail.com>
2023-09-09 22:18:36 +0600

    javascript

--- destructuring.js
+++ destructuring.js
@@ -13,11 +13,18 @@ const person = {
 const personName = person["name"];
 
 const {age : personAge} = person;
-console.log(personAge);
+//console.log(personAge);
 
 //print current address
 const {address : {currentAddress : bortomanThikana} = {}} = person;
-console.log(bortomanThikana);
+//console.log(bortomanThikana);
 
 //print persons name using destructuring
-console.log(personName);
+//console.log(personName);
+
+// array destructuirng 
+var numbers = [1, 2, 3, 4, [5, 55, 57, 59], 6, 7, 8, 9];
+
+var [,,,,[,,i]] = numbers;
+
+console.log(i);
